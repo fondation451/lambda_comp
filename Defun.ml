01effce057826be6dfafb0c91af08e7c52665705
Nicolas ASSOUAD <nicolas@assouad.com>
2018-01-19 14:03:14 +0100

    Optional Task : Eliminate variable to variable bindings

--- Defun.ml
+++ Defun.ml
@@ -107,7 +107,40 @@ let rec defun t evar efun apply lambda =
 
 let mk_apply apply = T.Fun(fun_apply, this_apply::that_apply, T.Swi(T.VVar(this_apply), apply));;
 
+(* Eliminate variable to variable bindings *)
+
+let rec subs esubs t =
+  match t with
+  |T.Exit -> t
+  |T.TailCall(var, v_l) -> T.TailCall(subs_var esubs var, List.map (subs_v esubs) v_l)
+  |T.Print(v, t1) -> T.Print(subs_v esubs v, subs esubs t1)
+  |T.LetVal(var, v, t1) -> begin
+    match v with
+    |T.VVar(var') -> subs (VarMap.add var (subs_var esubs var') esubs) t1
+    |_ -> T.LetVal(subs_var esubs var, subs_v esubs v, subs esubs t1)
+  end
+  |T.LetBlo(var, T.Con(tag, v_l), t1) -> T.LetBlo(subs_var esubs var, T.Con(tag, List.map (subs_v esubs) v_l), subs esubs t1)
+  |T.Swi(v, br_l) ->
+    T.Swi(
+      subs_v esubs v,
+      List.map (fun (T.Branch(tag, var_l, t1)) -> T.Branch(tag, List.map (subs_var esubs) var_l, subs esubs t1)) br_l
+    )
+  |T.IfZero(v, t1, t2) -> T.IfZero(subs_v esubs v, subs esubs t1, subs esubs t2)
+and subs_v esubs v =
+  match v with
+  |T.VVar(var) -> T.VVar(subs_var esubs var)
+  |T.VLit(_) -> v
+  |T.VBinOp(v1, op, v2) -> T.VBinOp(subs_v esubs v1, op, subs_v esubs v2)
+and subs_var esubs var =
+  try VarMap.find var esubs with Not_found -> var
+;;
+
+let eliminate (T.Prog(decl, t)) =
+  T.Prog(List.map (fun (T.Fun(var, var_l, t1)) -> T.Fun(var, var_l, subs VarMap.empty t1)) decl, subs VarMap.empty t)
+;;
+(* **** *)
+
 let defun_term (t : S.term) : T.program =
   let t', _, _, apply = defun t VarMap.empty VarMap.empty [] [] in
-  T.Prog([mk_apply apply], t')
+  eliminate (T.Prog([mk_apply apply], t'))
 ;;
